574481832da80b13f2158d584b1dfbaaa933abdd
123456789FH <212942390+123456789FH@users.noreply.github.com>
2026-08-14 21:32:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='school-improvement-v3.1.0';
+const CACHE='school-improvement-v3.2.0';
 const ASSETS=['./','index.html','manifest.json','assets/css/styles.css','assets/js/data.js','assets/js/storage.js','assets/js/engine.js','assets/js/app.js','assets/icons/icon-192.png','assets/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
